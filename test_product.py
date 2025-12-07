66a60c907dc0b5dff72b149d2aa0b179c548bbad
Shrinivas-go <shrinivas4927@gmail.com>
2025-12-07 09:44:19 +0530

    added all files

---
+++ test_product.py
@@ -0,0 +1,11 @@
+from products import product_details
+
+def test_product_details():
+  expected_output = (
+    "Product ID: 101\n"
+    "Product Name: iPhone\n"
+    "Quantity: 1\n"
+    "Price: 155000"
+  )
+
+  assert product_details(101, "iPhone", 1, 155000) == expected_output
